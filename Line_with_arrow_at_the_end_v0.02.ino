e794096cc0b5974f59f5c3f0e493b30ec3955883
Mike Morrow <15792417+MikeyMoMo@users.noreply.github.com>
2025-06-22 22:35:04 +0800

    Update Line_with_arrow_at_the_end_v0.02.ino
    Slight speedup for the triangle fill.

--- Line_with_arrow_at_the_end_v0.02.ino
+++ Line_with_arrow_at_the_end_v0.02.ino
@@ -3,6 +3,10 @@
 TFT_eSPI tft = TFT_eSPI();  // Invoke library
 TFT_eSprite mySprite = TFT_eSprite(&tft);
 #define RGB565(r,g,b) ((((r>>3)<<11) | ((g>>2)<<5) | (b>>3)))
+#define doBackLine true
+#define noBackLine false
+#define doTriangleFill true
+#define noTriangleFill false
 /***************************************************************************/
 void setup()
 /***************************************************************************/
@@ -30,58 +34,98 @@ void loop()
   //Note: PI / 6 is 30º and is a very natural looking angle for the arrow
   //       winglets.
   tft.fillScreen(TFT_BLACK);
-  drawArrowLine(&tft, 50, 50, 200, 120, TFT_RED, 12, PI / 6, TFT_WHITE, true);
+  drawArrowLine(&tft, 50, 50, 200, 120, TFT_RED, 12, PI / 6, TFT_WHITE,
+                true, true);
   drawArrowLine(&tft, random(tft.width()), random(tft.height()),
                 random(tft.width()), random(tft.height()),
-                TFT_GREEN, 12, PI / 6, TFT_WHITE, true);
+                TFT_GREEN, 12, PI / 6, TFT_WHITE, true, true);
   drawArrowLine(&tft, random(tft.width()), random(tft.height()),
                 random(tft.width()), random(tft.height()),
-                TFT_BLUE, 12, PI / 6, TFT_YELLOW, false);
+                TFT_BLUE, 12, PI / 6, TFT_YELLOW, false, true);
   drawArrowLine(&tft, random(tft.width()), random(tft.height()),
                 random(tft.width()), random(tft.height()),
-                TFT_RED, 12, PI / 6, TFT_WHITE, true);
+                TFT_RED, 12, PI / 6, TFT_WHITE, true, true);
   drawArrowLine(&tft, random(tft.width()), random(tft.height()),
                 random(tft.width()), random(tft.height()),
-                TFT_PURPLE, 12, PI / 6, TFT_YELLOW, false);
+                TFT_PURPLE, 12, PI / 6, TFT_YELLOW, false, true);
   drawArrowLine(&tft, random(tft.width()), random(tft.height()),
                 random(tft.width()), random(tft.height()),
-                TFT_RED, 12, PI / 6, TFT_WHITE, true);
+                TFT_RED, 12, PI / 6, TFT_WHITE, true, true);
+
+  drawArrowLine(&tft, 50, 50, 200, 120, TFT_RED, 12, PI / 6, TFT_WHITE,
+                true, false);
+  drawArrowLine(&tft, random(tft.width()), random(tft.height()),
+                random(tft.width()), random(tft.height()),
+                TFT_GREEN, 12, PI / 6, TFT_WHITE, true, false);
+  drawArrowLine(&tft, random(tft.width()), random(tft.height()),
+                random(tft.width()), random(tft.height()),
+                TFT_BLUE, 12, PI / 6, TFT_YELLOW, false, false);
+  drawArrowLine(&tft, random(tft.width()), random(tft.height()),
+                random(tft.width()), random(tft.height()),
+                TFT_RED, 12, PI / 6, TFT_WHITE, true, false);
+  drawArrowLine(&tft, random(tft.width()), random(tft.height()),
+                random(tft.width()), random(tft.height()),
+                TFT_PURPLE, 12, PI / 6, TFT_YELLOW, false, false);
+  drawArrowLine(&tft, random(tft.width()), random(tft.height()),
+                random(tft.width()), random(tft.height()),
+                TFT_RED, 12, PI / 6, TFT_WHITE, true, false);
   delay(10000);
 
-  mySprite.fillSprite(RGB565(25,25,25));
-  drawArrowLine(&mySprite, 50, 50, 200, 120, TFT_RED, 12, PI / 6, TFT_WHITE, true);
+  mySprite.fillSprite(RGB565(25, 25, 25));
+  drawArrowLine(&mySprite, 50, 50, 200, 120, TFT_RED, 12, PI / 6, TFT_WHITE,
+                true, true);
   drawArrowLine(&mySprite, random(tft.width()), random(tft.height()),
                 random(tft.width()), random(tft.height()),
-                TFT_GREEN, 12, PI / 6, TFT_WHITE, true);
+                TFT_GREEN, 12, PI / 6, TFT_WHITE, true, true);
   drawArrowLine(&mySprite, random(tft.width()), random(tft.height()),
                 random(tft.width()), random(tft.height()),
-                TFT_BLUE, 12, PI / 6, TFT_YELLOW, false);
+                TFT_BLUE, 12, PI / 6, TFT_YELLOW, false, true);
   drawArrowLine(&mySprite, random(tft.width()), random(tft.height()),
                 random(tft.width()), random(tft.height()),
-                TFT_RED, 12, PI / 6, TFT_WHITE, true);
+                TFT_RED, 12, PI / 6, TFT_WHITE, true, true);
   drawArrowLine(&mySprite, random(tft.width()), random(tft.height()),
                 random(tft.width()), random(tft.height()),
-                TFT_PURPLE, 12, PI / 6, TFT_YELLOW, false);
+                TFT_PURPLE, 12, PI / 6, TFT_YELLOW, false, true);
   drawArrowLine(&mySprite, random(tft.width()), random(tft.height()),
                 random(tft.width()), random(tft.height()),
-                TFT_RED, 12, PI / 6, TFT_WHITE, true);
+                TFT_RED, 12, PI / 6, TFT_WHITE, true, true);
+
+  drawArrowLine(&mySprite, 50, 50, 200, 120, TFT_RED, 12, PI / 6, TFT_WHITE,
+                true, false);
+  drawArrowLine(&mySprite, random(tft.width()), random(tft.height()),
+                random(tft.width()), random(tft.height()),
+                TFT_GREEN, 12, PI / 6, TFT_WHITE, true, false);
+  drawArrowLine(&mySprite, random(tft.width()), random(tft.height()),
+                random(tft.width()), random(tft.height()),
+                TFT_BLUE, 12, PI / 6, TFT_YELLOW, false, false);
+  drawArrowLine(&mySprite, random(tft.width()), random(tft.height()),
+                random(tft.width()), random(tft.height()),
+                TFT_RED, 12, PI / 6, TFT_WHITE, true, false);
+  drawArrowLine(&mySprite, random(tft.width()), random(tft.height()),
+                random(tft.width()), random(tft.height()),
+                TFT_PURPLE, 12, PI / 6, TFT_YELLOW, false, false);
+  drawArrowLine(&mySprite, random(tft.width()), random(tft.height()),
+                random(tft.width()), random(tft.height()),
+                TFT_RED, 12, PI / 6, TFT_WHITE, true, false);
   mySprite.pushSprite(0, 0);
   delay(10000);
 }
 /***************************************************************************/
-void drawArrowLine(TFT_eSPI* targetPallete,
+void drawArrowLine(TFT_eSPI* targetPallete,  // where line and arrow goes.
                    // Line start and end x & y
                    int x0, int y0, int x1, int y1,
-                   // Line color
+                   // Line color.
                    uint16_t lineColor,
-                   // Length of the little winglets to make the arrow
+                   // Length of the little winglets to make the arrow.
                    int arrowLength,
-                   // Angle away from the line for the two winglets
-                   float arrowAngle,
-                   // Color of the 2 or 3 arrow builder lines
+                   // Angle away from the line for the two winglets.
+                   float arrowAngle,  // 30º (PI / 6) looks good.
+                   // Color of the 2 or 3 arrow builder lines.
                    uint16_t arrowColor,
-                   // Do or do not draw back (3rd) line on arrow
-                   bool doBackLine)
+                   // Do or do not draw back (3rd) line on arrow.
+                   bool drawBackLine,
+                   // Do or do not fill in the triangle with arrow color.
+                   bool fillTheTriangle)
 /***************************************************************************/
 {
   static float angle;
@@ -93,15 +137,20 @@ void drawArrowLine(TFT_eSPI* targetPallete,
   angle = atan2(y1 - y0, x1 - x0);
 
   // Calculate arrowhead lines length and angles
-  // The arrowhead lines angles are based off of the original line angle.
+  // The arrowhead lines angles are offset by the original line angle.
   x2 = x1 - arrowLength * cos(angle - arrowAngle);
   y2 = y1 - arrowLength * sin(angle - arrowAngle);
   x3 = x1 - arrowLength * cos(angle + arrowAngle);
   y3 = y1 - arrowLength * sin(angle + arrowAngle);
 
-  // Draw arrowhead
-  targetPallete->drawLine(x1, y1, x2, y2, arrowColor);
-  targetPallete->drawLine(x1, y1, x3, y3, arrowColor);
-  // Do you want th0.0.e back line (3rd line) to complete the arrow?
-  if (doBackLine) targetPallete->drawLine(x2, y2, x3, y3, arrowColor);
+  if (fillTheTriangle) {
+    targetPallete->fillTriangle(x1, y1, x2, y2, x3, y3, arrowColor);
+  } else {
+    // Draw arrowhead
+    targetPallete->drawLine(x1, y1, x2, y2, arrowColor);
+    targetPallete->drawLine(x1, y1, x3, y3, arrowColor);
+    // Do you want the back line (3rd line) to complete the arrow?
+    if (drawBackLine) targetPallete->drawLine(x2, y2, x3, y3, arrowColor);
+    // Do you want the triangle filled with the triangle line color?
+  }
 }
